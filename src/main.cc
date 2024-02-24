593b379cc390cea6713a47b5dd77e3f3d7ca8099
mcufari <mcufari@syr.edu>
2024-02-24 14:49:30 -0500

    added some statements to print updates

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,10 @@
 #include "../include/camera.hpp"
+#include <iostream>
 int main(){
     Camera cam;
     Grid g;
+    std::cout << "begin trace " << std::endl;
     cam.beginRayTrace(g);
-    cam.outRayTrace();
+    std::cout << "End trace" << std::endl;
     return 0;
 }
