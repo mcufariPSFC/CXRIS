2634bb3574367c0009e39a2f16a789cc5dca9cc0
mcufari <mcufari@syr.edu>
2024-02-25 18:39:24 -0500

    added bounding shell collection geo, which refers to the collection of geometry created for each nr

---
+++ include/boundingShellCollection.cpp
@@ -0,0 +1,30 @@
+#include "boundingShellCollection.hpp"
+#include <cmath>
+double M_PI3_4 = 3.0 * M_PI_4;
+BoundingShellCollection::BoundingShellCollection(double rin, double rout, int maxRefinei):
+inR(rin),
+outR(rout),
+maxRefine(maxRefinei)
+{
+    firstOct = new BoundingShell(inR, outR, maxRefine, 0, M_PI_4, M_PI_4);
+    secondOct = new BoundingShell(inR, outR, maxRefine, 0, M_PI_4, 3.0 * M_PI_4);
+    thirdOct = new BoundingShell(inR, outR, maxRefine, 0, M_PI_4, 5.0 * M_PI_4);
+    fourthOct = new BoundingShell(inR, outR, maxRefine, 0, M_PI_4, 7.0 * M_PI_4);
+
+
+    fifthOct = new BoundingShell(inR, outR, maxRefine, 0, M_PI3_4, M_PI_4);
+    sixthOct = new BoundingShell(inR, outR, maxRefine, 0, M_PI3_4, M_PI3_4);
+    seventhOct = new BoundingShell(inR, outR, maxRefine, 0, M_PI3_4, 5.0 * M_PI_4);
+    eightOct = new BoundingShell(inR, outR, maxRefine, 0, M_PI3_4, 7.0 * M_PI_4);
+}
+
+void BoundingShellCollection::print_memberCoords(int levelDes){
+    firstOct->print_memberCoords(levelDes);
+    secondOct->print_memberCoords(levelDes);
+    thirdOct->print_memberCoords(levelDes);
+    fourthOct->print_memberCoords(levelDes);
+    fifthOct->print_memberCoords(levelDes);
+    sixthOct->print_memberCoords(levelDes);
+    seventhOct->print_memberCoords(levelDes);
+    eightOct->print_memberCoords(levelDes);
+}
---
+++ include/boundingShellCollection.hpp
@@ -0,0 +1,27 @@
+#ifndef BOUNDINGSHELLCOLLECTION_H
+#define BOUNDINGSHELLCOLLECTION_H
+#include "boundingShell.hpp"
+
+
+class BoundingShellCollection{
+public:
+    
+    BoundingShellCollection(double rin, double rout, int maxRefine);
+    void print_memberCoords(int levelDes);
+
+private:
+    double inR;
+    double outR;
+    int maxRefine;
+
+    BoundingShell* firstOct;
+    BoundingShell* secondOct;
+    BoundingShell* thirdOct;
+    BoundingShell* fourthOct;
+    BoundingShell* fifthOct;
+    BoundingShell* sixthOct;
+    BoundingShell* seventhOct;
+    BoundingShell* eightOct;
+};
+
+#endif
